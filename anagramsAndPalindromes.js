7ffec457cb031a56165655290e54cce4c9f7c44a
Joe Mercer <mynameisjoemercer@gmail.com>
2014-02-02 15:42:26 -0500

    basic anagrams and palindromes

---
+++ anagramsAndPalindromes.js
@@ -0,0 +1,94 @@
+var checkAnagram = function(a, b) {
+	if (a.length !== b.length) {
+		// => obviously can't be an anagram
+		return false;
+	}
+
+	var aChars = a.split('');
+	var aCharMap = {};
+	aChars.forEach(function(aChar){
+		if (aCharMap[aChar]) {
+			aCharMap[aChar] = aCharMap[aChar] + 1;
+		}
+		else {
+			aCharMap[aChar] = 1;
+		}
+	});
+
+	var bChars = b.split('');
+	for (var i=0; i<bChars.length; ++i) {
+		if (aCharMap[bChar]) {
+			aCharMap[bChar] = aCharMap[bChar] - 1;
+			if (aCharMap[bChar] < 0) {
+				// => b contains more of a type of character than a
+				return false;
+			}
+		}
+		else {
+			// => b contains a character a doesn't
+			return false;
+		}
+	}
+
+	return true;
+}
+
+var checkPalindrome = function(a) {
+	var min = 0;
+	var max = a.length-1;
+
+	while (min < max) {
+		if (a[min] !== a[max]) {
+			// => kth character doesn't equal n-kth character
+			return false;
+		}
+		min = min+1;
+		max = max-1;
+	}
+
+	return true;
+}
+
+// run it
+var tests = [
+	{
+		input: 'sam',
+		output: false
+	},
+	{
+		input: 'sas',
+		output: true
+	},
+	{
+		input: 'saas',
+		output: true
+	},
+	{
+		input: 'satas',
+		output: true
+	},
+	{
+		input: 'sattas',
+		output: true
+	},
+	{
+		input: 'satlas',
+		output: true
+	}
+];
+
+tests.forEach(function(test){
+	console.log(test.input);
+	console.log(checkPalindrome(test.input));
+});
+
+
+
+
+
+
+
+
+
+
+
